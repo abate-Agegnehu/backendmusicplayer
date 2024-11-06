a629804e75112cf91fc2e00378322125cf817b9c
Abate <147659641+abate-Agegnehu@users.noreply.github.com>
2024-11-06 17:05:39 +0300

    backend music player

---
+++ utils/multer.js
@@ -0,0 +1,16 @@
+
+const multer = require("multer");
+const path = require("path");
+
+module.exports = multer({
+  storage: multer.diskStorage({}),
+  fileFilter: (req, file, cb) => {
+    let ext = path.extname(file.originalname).toLowerCase();
+    if (ext !== ".mp4" && ext !== ".avi" && ext !== ".mov" && ext !== ".mkv") {
+      cb(new Error("File type is not supported"), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
+
